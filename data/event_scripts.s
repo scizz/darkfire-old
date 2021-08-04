57d7549de27a49eeae4f62f56db05d17132b9d4e
Karl <Karl_Harmer@hotmail.com>
2021-08-04 11:03:35 +0100

    added route 4 + connections

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1093,3 +1093,5 @@ Common_EventScript_LegendaryFlewAway:: @ 8273776
 	.include "data/maps/Route2/scripts.inc"
 
 	.include "data/maps/Route3/scripts.inc"
+
+	.include "data/maps/Route4/scripts.inc"
