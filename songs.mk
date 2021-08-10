aa0b93e6fc4308b314b7d6a9c484a4096a8f0b4e
W1serV1ser <1prefertheaircauseimanairnomad@gmail.com>
2021-08-10 12:09:47 -0700

    Fixed mus_chandra_village.s creation error, changed instrument names in custom voicegroup to proper names.

--- songs.mk
+++ songs.mk
@@ -1264,4 +1264,4 @@ $(MID_SUBDIR)/se_dex_search.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G127 -v100 -P5
 	
 $(MID_SUBDIR)/mus_chandra_village.s: %.s: %.mid
-	$(MID) $> $@ -E -G191 -V127
+	$(MID) $< $@ -E -G191 -V127
